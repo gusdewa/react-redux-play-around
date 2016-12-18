b1bcc74fd64ab53a1ab27669ebe943eb729df548
Rahadian Dewandono <gusdewa@gmail.com>
2016-12-19 05:20:38 +0800

    Initial commits

---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import 'babel-polyfill';
+
+import 'ts-helpers';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+import routes from './store/routes';
+import configureStore from './store/configure-store';
+
+import './styles/index.css';
+
+declare const __TEST__: boolean;
+
+const store = configureStore({});
+const history = syncHistoryWithStore(browserHistory, store);
+
+if (!__TEST__) {
+  ReactDOM.render(
+    <div>
+      <Provider store={ store }>
+        <Router history={ history }>
+          { routes }
+        </Router>
+      </Provider>
+    </div>,
+    document.getElementById('root')
+  );
+}
